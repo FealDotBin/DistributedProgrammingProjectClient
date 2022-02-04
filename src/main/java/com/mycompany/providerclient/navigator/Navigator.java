17213f5641dda1f005557f43adcf77a5a1c2f4b8
FealDotBin <aferrentino96@gmail.com>
2022-02-04 19:13:10 +0100

    [+] Added javadoc to Navigator

--- src/main/java/com/mycompany/providerclient/navigator/Navigator.java
+++ src/main/java/com/mycompany/providerclient/navigator/Navigator.java
@@ -12,89 +12,140 @@ import com.mycompany.providerclient.controller.SignUpController;
 import com.mycompany.providerclient.controller.UpdateController;
 
 /**
- *
+ * Represents a navigator between the views. It allows the view-switch, disposing
+ * the previous view and creating a new controller instance of the next view.
  * @author aferr
  */
 public class Navigator {
     
     private static Navigator instance;
         
-        private Navigator(){
-            
+    private Navigator(){}
+    
+    /**
+     * Factory method to return the only instance of navigator
+     * @return the instance of navigator
+     */
+    public synchronized static Navigator getInstance() {
+        if (instance == null) {
+            instance = new Navigator();
         }
-        
-        public synchronized static Navigator getInstance() {
-            if (instance == null) {
-                instance = new Navigator();
-            }
-            return instance;
-        }
-        
-        public void fromLogInToHome(LogInController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new HomeController(providerId);
-        }
-        
-        public void fromLogInToSignUp(LogInController c){
-            c.disposeView();
-            new SignUpController();
-        }
-        
-        public void fromSignUpToLogIn(SignUpController c){
-            c.disposeView();
-            new LogInController();
-        }
-        
-        public void fromHomeToMenuManager(HomeController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new MenuManagerController(providerId);
-        }
-        
-        public void fromHomeToUpdateAccount(HomeController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new UpdateController(providerId);
-        }
-        
-        public void fromHomeToLogIn(HomeController c){
-            c.disposeView();
-            new LogInController();
-        }
-        
-        public void fromMenuManagerToHome(MenuManagerController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new HomeController(providerId);
-        }
-        
-        public void fromMenuManagerToUpdateAccount(MenuManagerController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new UpdateController(providerId);
-        }
-        
-        public void fromMenuManagerToLogIn(MenuManagerController c){
-            c.disposeView();
-            new LogInController();
-        }
-        
-        public void fromUpdateToMenuManager(UpdateController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new MenuManagerController(providerId);
-        }
-        
-        public void fromUpdateToHome(UpdateController c){
-            c.disposeView();
-            Long providerId = c.getProviderId();
-            new HomeController(providerId);
-        }
-        
-        public void fromUpdateToLogIn(UpdateController c){
-            c.disposeView();
-            new LogInController();
-        }
+        return instance;
+    }
+
+    /**
+     * Switch the view from LogInView to HomeView
+     * @param c the LogInController instance
+     */
+    public void fromLogInToHome(LogInController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new HomeController(providerId);
+    }
+
+    /**
+     * Switch the view from LogInView to SignUpView
+     * @param c the LogInController instance
+     */
+    public void fromLogInToSignUp(LogInController c){
+        c.disposeView();
+        new SignUpController();
+    }
+
+    /**
+     * Switch the view from SignUpView to LogInView
+     * @param c the SignUpController instance
+     */
+    public void fromSignUpToLogIn(SignUpController c){
+        c.disposeView();
+        new LogInController();
+    }
+
+    /**
+     * Switch the view from HomeView to MenuManagerView
+     * @param c the HomeController instance
+     */
+    public void fromHomeToMenuManager(HomeController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new MenuManagerController(providerId);
+    }
+
+    /**
+     * Switch the view from HomeView to UpdateView
+     * @param c the HomeController instance
+     */
+    public void fromHomeToUpdateAccount(HomeController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new UpdateController(providerId);
+    }
+
+    /**
+     * Switch the view from HomeView to LogInView
+     * @param c the HomeController instance
+     */
+    public void fromHomeToLogIn(HomeController c){
+        c.disposeView();
+        new LogInController();
+    }
+
+    /**
+     * Switch the view from MenuManagerView to HomeView
+     * @param c the MenuManagerController instance
+     */
+    public void fromMenuManagerToHome(MenuManagerController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new HomeController(providerId);
+    }
+
+    /**
+     * Switch the view from MenuManagerView to UpdateView
+     * @param c the MenuManagerController instance
+     */
+    public void fromMenuManagerToUpdateAccount(MenuManagerController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new UpdateController(providerId);
+    }
+
+    /**
+     * Switch the view from MenuManagerView to LogInView
+     * @param c the MenuManagerController instance
+     */
+    public void fromMenuManagerToLogIn(MenuManagerController c){
+        c.disposeView();
+        new LogInController();
+    }
+
+    /**
+     * Switch the view from UpdateView to MenuManagerView
+     * @param c the UpdateController instance
+     */
+    public void fromUpdateToMenuManager(UpdateController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new MenuManagerController(providerId);
+    }
+
+    /**
+     * Switch the view from UpdateView to HomeView
+     * @param c the UpdateController instance
+     */
+    public void fromUpdateToHome(UpdateController c){
+        c.disposeView();
+        Long providerId = c.getProviderId();
+        new HomeController(providerId);
+    }
+
+    /**
+     * Switch the view from UpdateView to LogInView
+     * @param c the UpdateController instance
+     */
+    public void fromUpdateToLogIn(UpdateController c){
+        c.disposeView();
+        new LogInController();
+    }
         
 }
