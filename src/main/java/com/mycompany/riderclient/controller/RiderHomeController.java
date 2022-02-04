8ded5893f9d6eda3e54c44e11ae6a16c505583c8
amosbattista <a.battista16@studenti.unisa>
2022-02-04 18:27:21 +0100

    rider doc

--- src/main/java/com/mycompany/riderclient/controller/RiderHomeController.java
+++ src/main/java/com/mycompany/riderclient/controller/RiderHomeController.java
@@ -35,7 +35,17 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- *
+ * This class represents the controller of the view RiderHomeView. 
+ * Allows to control and manage all the GUI components.
+ * It also manages the logic needed to call the APIs asynchronously, based on the user request on the GUI.
+ * 
+ * The controller, given the rider ID, allows you to perform the following operations:
+ * - view acceptable orders
+ * - view accepted orders
+ * - log out using the menu
+ * - open the update-form using the menu
+ * - accept an order using button 
+ * - show an accepted order using button
  * @author Amos
  */
 public class RiderHomeController {
@@ -65,56 +75,73 @@ public class RiderHomeController {
         homeView.dispose();
     }
      
+     /**
+      * Initialize:
+      * - the view and its components;
+      * - the retrofitBuilder and the serviceApi in order to invoke the API calls.
+      * - the Navigator to switch between views
+      * - attach listeners to button, radio-button and menu items in order to manage events
+      * 
+      * @param riderId is the rider ID that uniquely identifies the rider
+      */
      public RiderHomeController(Long riderId){
         this.riderId = riderId;
-        homeView = new RiderHomeView();
+        
+        //initialize view
+        homeView = new RiderHomeView(); 
         homeView.setVisible(true);
         
-        
+        // initizialize view components
         semiAcceptedRadio = homeView.getRadioSemiAcc();
         acceptRadio = homeView.getRadioAccepted();
         semiAcceptedRadio.setSelected(true);
         acceptBtn = homeView.getAcceptBtn();
         showBtn = homeView.getViewOrderBtn();
-
         showBtn.setEnabled(false);
         orderTable = homeView.getRiderTable();
         
-        btnGroup = homeView.getBtnRadioGroup();
-        
+        btnGroup = homeView.getBtnRadioGroup();        
         updateItem = homeView.getMyInfoItem();
         logOutItem = homeView.getLogoutItem();
         
         
-               // initialize retrofitBuilder and serviceApi
+        // initialize retrofitBuilder and serviceApi
         retroBuild = new RetrofitBuilder();
         apiService = retroBuild.getRetrofit().create(ServiceApi.class);
         
+        // initialize Navigator to switch between views
         navigator = Navigator.getInstance();
         
         
-       acceptBtn.addMouseListener(acceptOrderAction); 
-       showBtn.addMouseListener(showOrderAction);           
-       acceptRadio.addActionListener(acceptRadioAction);
-       semiAcceptedRadio.addActionListener(semiAcceptRadioAction);
-       
-       updateItem.addActionListener(updateAction);
-       
-       logOutItem.addActionListener(logoutAction);
+        // add listeners to button, radio-button and menu items
+        acceptBtn.addMouseListener(acceptOrderAction); 
+        showBtn.addMouseListener(showOrderAction);           
+        acceptRadio.addActionListener(acceptRadioAction);
+        semiAcceptedRadio.addActionListener(semiAcceptRadioAction);
+        updateItem.addActionListener(updateAction);
+        logOutItem.addActionListener(logoutAction);
         
+        // fill the table on the view
         fillOrderTable();
      }
      
-     
-    public void fillOrderTable(){
+   
+    /**
+     * This method allows to fill the fields of the order table. 
+     * The method will call an asynchronous api in order to get the order list. 
+     * The type of call depend on how the user has selected the Radio Button
+     */
+    private void fillOrderTable(){
          
-          Call<List<OrderDto>> call = null;
-         if(semiAcceptedRadio.isSelected()){         
-             call = apiService.getSemiAcceptedOrders();
-         }
-         else if(acceptRadio.isSelected()){
-             call = apiService.getAtLeastAccepted(riderId);
-         }
+        Call<List<OrderDto>> call = null;
+        if(semiAcceptedRadio.isSelected()){         
+            call = apiService.getSemiAcceptedOrders();
+        }
+        else if(acceptRadio.isSelected()){
+            call = apiService.getAtLeastAccepted(riderId);
+        }
+        
+        //API CALL
         
         call.enqueue(new Callback<List<OrderDto>>() {
             @Override
@@ -154,9 +181,6 @@ public class RiderHomeController {
                     JOptionPane.showMessageDialog(homeView, jObjError.get("message"), "Server error", JOptionPane.ERROR_MESSAGE);
                 }
                 
-              
-                
-                
             }
 
             @Override
@@ -173,7 +197,13 @@ public class RiderHomeController {
         
      }
      
+    /**
+     * It's a mouse listener that allows you to accept an order selected
+     * in the table of acceptable orders.
+     * The method call an asynchronous API to accept the order
+     */
     private MouseAdapter acceptOrderAction = new MouseAdapter() {
+        
         @Override
         public void mouseClicked(MouseEvent mouseEvent) {
             
@@ -183,22 +213,23 @@ public class RiderHomeController {
             if(!acceptBtn.isEnabled())
                 return;
             
-            if(selectedRow==-1){ //se ha selezionato almeno una riga nella tabella semiaccepted
+            if(selectedRow==-1){ //if one row is selected on the table
                 JOptionPane.showMessageDialog(homeView,
                                 "Order not selected!",
                                 "CRITICAL ERROR",
                                 JOptionPane.ERROR_MESSAGE);
                 return;
-            }
+            }            
             
-            //recuperare Id dalla tabella - 30L
-             Call<Void> call2 = apiService.acceptOrder(riderId,(Long) orderTable.getModel().getValueAt(selectedRow, 0));
-             
-              call2.enqueue(new Callback<Void>() {
+            
+            //API CALL
+            
+            Call<Void> call2 = apiService.acceptOrder(riderId,(Long) orderTable.getModel().getValueAt(selectedRow, 0));             
+            call2.enqueue(new Callback<Void>() {
                   
-                   @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                if (response.isSuccessful()) { // status code tra 200-299
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) { // status code tra 200-299
+                if (response.isSuccessful()) { 
                         fillOrderTable();
                           JOptionPane.showMessageDialog(homeView, "Order accepted succesfully", "Order shipped", JOptionPane.INFORMATION_MESSAGE);
                       }
@@ -226,17 +257,20 @@ public class RiderHomeController {
             }
                   
               });
-          
-        
             
         }};
-    
-    
-     private MouseAdapter showOrderAction = new MouseAdapter() {
+        
+    /**
+     * It's a mouse listener that allows you to show an order selected
+     * in the table of accepted orders.
+     * It calls a navigator method in order to pass the control to the next view,
+     * controlled by RiderOrderController
+     */
+    private MouseAdapter showOrderAction = new MouseAdapter() {
         @Override
         public void mouseClicked(MouseEvent mouseEvent) {
             
-            int selectedRow = orderTable.getSelectedRow();
+            int selectedRow = orderTable.getSelectedRow(); //if one row is selected on the table
             if(!showBtn.isEnabled())
                 return;
             
@@ -251,12 +285,14 @@ public class RiderHomeController {
             
             navigator.fromHomeToOrderView(RiderHomeController.this, (Long) orderTable.getModel().getValueAt(selectedRow, 0));
             
-            //col navigator passare a RiderOrderController
-            
         }
         
      };
     
+    /**
+     * It's an action listener that enable/disable buttons
+     * based on the radio button chosen by the user.
+     */
     private ActionListener acceptRadioAction = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {            
@@ -264,33 +300,39 @@ public class RiderHomeController {
             showBtn.setEnabled(true);
             fillOrderTable();
         }
-        
-        
-        
+
     };
     
-     private ActionListener updateAction = new ActionListener(){
+    /**
+     * It's an action listener that calls a navigator method in order to open the update View,
+     * controlled by RiderUpdateController. It's connected to the UpdateItem.
+     */
+    private ActionListener updateAction = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {            
             navigator.showUpdateView(riderId);
         }
         
         
-        
     };
      
-        private ActionListener logoutAction = new ActionListener(){
+    /**
+     * It's an action listener that calls a navigator method in order to the next View,
+     * controlled by RiderLoginController. It's connected to the LogoutItem.
+     */
+    private ActionListener logoutAction = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {            
             navigator.fromHomeToLogin(RiderHomeController.this);
         }
         
-        
-        
     };
     
     
-     
+   /**
+     * It's an action listener that enable/disable buttons
+     * based on the radio button chosen by the user.
+     */
     private ActionListener semiAcceptRadioAction = new ActionListener(){
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -304,16 +346,6 @@ public class RiderHomeController {
     public Long getRiderId() {
         return riderId;
     }
-  
-  
-     
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        RiderHomeController contr = new RiderHomeController(35L);
-    }
     
 }
