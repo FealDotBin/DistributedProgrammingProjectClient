9da4817d3977dea1e021478b03c91609e9e480ae
FealDotBin <aferrentino96@gmail.com>
2022-02-05 11:16:31 +0100

    [+] Added javadoc to LogInController

--- src/main/java/com/mycompany/providerclient/controller/LogInController.java
+++ src/main/java/com/mycompany/providerclient/controller/LogInController.java
@@ -25,7 +25,14 @@ import java.util.logging.Logger;
 import org.json.JSONObject;
 
 /**
- *
+ * This class represents LogInView GUI controller. 
+ * This class is responsible for capturing input from the user and updating the view.
+ * It also makes calls to the restful server when needed:
+ * <ul>
+ *   <li> To verify provider credentials </li>
+ *   <li> To decide which controller to invoke after that provider
+ *   successfully log in </li>
+ * </ul>
  * @author aferr
  */
 public class LogInController {
@@ -40,6 +47,11 @@ public class LogInController {
     private Long providerId;
     private Navigator navigator;
     
+    /**
+     * Add listener to update button to log in a provider
+     * Determine the next interface to show after a successful login communicating with the server
+     * Add listener to sign up button to navigate to singUp view
+     */
     public LogInController() {
         // initialize view
         logInView = new LogInView();
@@ -63,14 +75,28 @@ public class LogInController {
         attachListenerToSignUpBtn();
     }
 
+    /**
+     * Getter for providerId
+     * @return the provider's id
+     */
     public Long getProviderId(){
         return providerId;
     }
     
+    /**
+     * Destroy the logInView instance and all its components.
+     */
     public void disposeView(){
         logInView.dispose();
     }
     
+    /**
+     * Send a POST request to the server to authenticate the provider sending
+     * the provided credentials.
+     * If the response is successful, the provider is logged in and he is
+     * redirected to the home page;
+     * otherwise, an error message is shown to the user.
+     */
     private void loginCall(Credentials credentials){
         Call<Long> loginCall = serviceApi.login(credentials);
         loginCall.enqueue(new Callback<Long>(){
@@ -106,6 +132,10 @@ public class LogInController {
         });
     }
     
+    /**
+     * Attach an ActionListener to logInButton in order to authenticate
+     * the provider, when the button is pressed
+     */
     private void attachListenerToLogInBtn(){
         logInBtn.addActionListener((event) -> {
             String username = usernameTextField.getText(true).trim();
@@ -135,12 +165,20 @@ public class LogInController {
         });
     }
     
+    /**
+     * Attach an ActionListener to signUpButton in order to switch to
+     * the "sign up" window, when the button is pressed.
+     */
     private void attachListenerToSignUpBtn(){
         signUpBtn.addActionListener((event) -> {
             navigator.fromLogInToSignUp(LogInController.this);
         });
     }
     
+    /**
+     * Create a new LogInController
+     * @param args 
+     */
     public static void main(String args[]) {
        SwingUtilities.invokeLater(new Runnable() {
            @Override
